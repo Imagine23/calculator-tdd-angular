79f3c9848fcea911e70cc3b95a6f46923eff0e7f
mfaisal.pixel6 <faisal@pixel6.co>
2024-09-12 21:09:54 +0530

    implent add method in service for empty string and test cases for empty and single number

---
+++ calculator/src/app/calculator.service.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CalculatorService } from './calculator.service';
+
+describe('CalculatorService', () => {
+  let service: CalculatorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CalculatorService);
+  });
+
+  it('should return 0 for empty string', () => {
+    const result = service.add('');
+    expect(result).toEqual(0);
+  });
+
+  it('should return number itself if the input is a single number', ()=> {
+    const result = service.add('1');
+    expect(result).toEqual(1);
+  })
+});
